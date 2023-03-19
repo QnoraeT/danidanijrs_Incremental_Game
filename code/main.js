cf70710265718e4e703d42d8a86b631155e597e8
Yoreni <yoreni@icloud.com>
2023-03-19 10:08:14 +0100

    removed that border

--- code/main.js
+++ code/main.js
@@ -1,5 +1,4 @@
-//the codes layout and a little bit of the actual code is from kittykatattacks learning pixi.js tutorial i also used pixi.js and collections.js
-
+let canvasSize = {width: 0, height: 0}
 
 const textures = PIXI.Assets.load(["images/on.png","images/off.png","danidanijrs-font.ttf"])
     .then(function()
@@ -7,6 +6,27 @@ const textures = PIXI.Assets.load(["images/on.png","images/off.png","danidanijrs
         console.log("Textures Loaded");
         setup();
     });
+
+window.addEventListener('resize', resize);
+    
+function resize()
+{
+        var h = 640;
+        var width = window.innerWidth || document.body.clientWidth; 
+        var height = window.innerHeight || document.body.clientHeight; 
+        var ratio = height / h;
+        var view = app.view;
+    
+        view.style.height = (h * ratio) + "px";
+        var newWidth = (width / ratio);
+        view.style.width = width + "px";
+        
+        app.renderer.resize(newWidth , h);
+    
+        canvasSize.width = newWidth
+        canvasSize.height = h
+        console.log(canvasSize)
+}
 
 function dev()
 {
@@ -23,6 +43,9 @@ function dev()
 
 function setup()
 {   
+    resize();
+    app.ticker.maxFPS = 60
+
     //header
     pointsDisplay = new PIXI.Text("0.000",danidanijrStyle())
     pointsDisplay.position.set(0, 5)
